13ca2a1910515ee794f0203246c584b41a8727d5
pc <nikita.shevnin2016@yandex.ru>
2024-09-07 20:06:41 +0300

    ver 1.4 Solution

--- src/main/java/solution/myArrayListRealisation.java
+++ src/main/java/solution/myArrayListRealisation.java
@@ -1,56 +1,39 @@
 package solution;
 
-/**
- * A custom implementation of a non-thread-safe ArrayList.
- *
- * @param <T> the type of elements in this list
- */
-public class myArrayListRealisation<T extends Comparable<T>> {
+import java.util.Arrays;
 
+/**
+ * A simple implementation of an ArrayList-like data structure that can store elements of a
+ * parameterized type T.
+ *
+ * @param <T> the type of elements stored in the list
+ */
+public class myArrayListRealisation<T> {
     private T[] array;
     private int size;
 
-    public myArrayListRealisation() {
-        this.array = (T[]) new Object[10]; // Инициализируем массив с начальным размером 10
-        this.size = 0;
-    }
-
     /**
-     * Constructs an empty list with an initial capacity of 10.
+     * Constructs a new {@code myArrayListRealisation} instance with an initial capacity of 10.
      */
-    public void MyArrayList() {
-        this.array = new T[10];
+    public myArrayListRealisation() {
+        this.array = (T[]) new Object[10];
         this.size = 0;
     }
 
     /**
      * Adds the specified element to the end of this list.
-     * If the list is full, it increases the capacity of the underlying array.
      *
      * @param element the element to be added to this list
      */
     public void add(T element) {
-        if (size == array.length) { // Если массив заполнен
-            T[] newArray = (T[]) new Object[array.length * 2]; // Увеличиваем размер массива в 2 раза
+        if (size == array.length) {
+            T[] newArray = (T[]) new Object[array.length * 2];
             for (int i = 0; i < array.length; i++) {
                 newArray[i] = array[i];
             }
-            array = newArray; // Присваиваем новый массив
+            array = newArray;
         }
-        array[size++] = element; // Добавляем элемент в массив
-    }
-
-
-    /**
-     * Increases the capacity of this ArrayList instance, if necessary, to ensure
-     * that it can hold at least the number of elements specified by the
-     * minimum capacity argument.
-     */
-    private void increaseCapacity() {
-        int newCapacity = array.length * 2;
-        Object[] newArray = new Object[newCapacity];
-        System.arraycopy(array, 0, newArray, 0, array.length);
-        array = newArray;
+        array[size++] = element;
     }
 
     /**
@@ -60,44 +43,28 @@ public class myArrayListRealisation<T extends Comparable<T>> {
      * @return the element at the specified position in this list
      * @throws IndexOutOfBoundsException if the index is out of range (index < 0 || index >= size())
      */
-    @SuppressWarnings("unchecked")
     public T get(int index) {
         if (index < 0 || index >= size) {
-            throw new IndexOutOfBoundsException();
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
         }
-        return (T) array[index];
+        return array[index];
     }
 
     /**
      * Replaces the element at the specified position in this list with the specified element.
      *
-     * @param index   the index of the element to replace
-     * @param element the element to be stored at the specified position
+     * @param index    the index of the element to replace
+     * @param element  the element to be stored at the specified position
+     * @return the element previously at the specified position
      * @throws IndexOutOfBoundsException if the index is out of range (index < 0 || index >= size())
      */
-
-    public void set(int index, T element) {
+    public T set(int index, T element) {
         if (index < 0 || index >= size) {
-            throw new IndexOutOfBoundsException();
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
         }
+        T oldValue = array[index];
         array[index] = element;
-    }
-
-    /**
-     * Removes the element at the specified position in this list.
-     * Shifts any subsequent elements to the left (subtracts one from their indices).
-     *
-     * @param index the index of the element to be removed
-     * @throws IndexOutOfBoundsException if the index is out of range (index < 0 || index >= size())
-     */
-    public void remove(int index) {
-        if (index < 0 || index >= size) {
-            throw new IndexOutOfBoundsException();
-        }
-        for (int i = index; i < size - 1; i++) {
-            array[i] = array[i + 1];
-        }
-        size--;
+        return oldValue;
     }
 
     /**
@@ -107,5 +74,21 @@ public class myArrayListRealisation<T extends Comparable<T>> {
      */
     public int size() {
         return size;
+    }
+
+    /**
+     * Returns a string representation of this {@code myArrayListRealisation} instance.
+     * The string representation consists of a list of the elements, enclosed in curly braces
+     * ("{}"). Adjacent elements are separated by the characters ", " (a comma followed by a
+     * space). Elements are converted to strings as by {@link String#valueOf(Object)}.
+     *
+     * @return a string representation of this list
+     */
+    @Override
+    public String toString() {
+        return "myArrayListRealisation{" +
+                "array=" + Arrays.toString(array) +
+                ", size=" + size +
+                '}';
     }
 }
